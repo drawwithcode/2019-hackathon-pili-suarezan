8c7881f01cbf6f848e476c483f44f8d8c7a0a155
Maria del Pilar Suarez Anzorena <pili.suarezan@gmail.com>
2019-11-27 12:40:36 +0100

    new

--- sketch.js
+++ sketch.js
@@ -1,11 +1,56 @@
-function preload(){
-  // put preload code here
+var myImage1;
+var mySound;
+var analyzer;
+
+
+function preload() {
+  mySound = loadSound("./assets/TG1_bumper.mp3");
+  myImage1 = loadImage("./assets/dance1.jpg");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background("white");
+  imageMode(CENTER);
+
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySound);
+
+  mySound.pause();
+  mySound.playMode('restart');
+
 }
 
 function draw() {
-  // put drawing code here
+//Image
+image(myImage1, width / 2, height / 2, myImage1.With / 1.2, myImage1.height / 1.2);
+
+//Sound map
+    var volume = 0;
+
+    volume = analyzer.getLevel();
+    volume = map(volume,0,1,0, height);
+    fill(255,230,0,100);
+    noStroke();
+    ellipse(width / 2+220, height / 5+430, volume);
+
+    //text
+    fill("gray");
+    textFont("Arial");
+    textSize(30);
+    text("Push the play button", width /2+135, height /5+650 );
 }
+
+  function mouseClicked() {
+    if (mySound.isPlaying() == false) {
+      mySound.play();
+    } else {
+      mySound.pause();
+    }
+  }
+
+
+  //WINDOW RESIZED
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
